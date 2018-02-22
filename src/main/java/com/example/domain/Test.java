1084a4e07ebb79584d33068c9cf9937687708df8
quyanhang1 <3169119846@qq.com>
2018-02-23 02:34:34 +0800

    Commit Message

--- src/main/java/com/example/domain/Test.java
+++ src/main/java/com/example/domain/Test.java
@@ -4,4 +4,7 @@ package com.example.domain;
  * Created by Administrator on 2018/2/23.
  */
 public class Test {
+    public static void main(String[] args){
+        System.out.println("ceshi");
+    }
 }
